80c5ba0fd117c708c9e13b145b7db161dcc3dea5
Prosper <prosperekakati@gmail.com>
2025-04-05 13:05:26 +0100

    Initial commit

---
+++ RandomPicks/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace RandomPicks
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var random = new Random();
+            int randomNumber = random.Next(1, 101);
+
+
+            Console.WriteLine("Pick a number between 1 and 100");
+            string number = Console.ReadLine();
+
+            Console.WriteLine("\n Random number is: " + randomNumber);
+
+            if (randomNumber > Convert.ToInt32(number))
+            {
+                Console.WriteLine("Number is low");
+            }
+            if (randomNumber < int.Parse(number))
+            {
+                Console.WriteLine("Number is high");
+            }
+
+            if (int.TryParse(number, out int numberToInt) && randomNumber == numberToInt)
+            {
+                Console.WriteLine("Nice one! You got the number correctly");
+            }
+
+
+            /* else
+             {
+                 Console.WriteLine("Try again");
+             } */
+
+
+            /*if (numberInt < randomNumber)
+            {
+                Console.WriteLine("Number is low");
+            }
+            if (numberInt == numberInt)
+            {
+                Console.WriteLine("Nice one! You got the number correctly");
+            }
+             */
+        }
+    }
+}
+
